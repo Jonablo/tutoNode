1326a317b1c02695abc9e561e2b7d0009078f662
Jonablo <138418199+Jonablo@users.noreply.github.com>
2024-12-01 12:48:00 -0500

    test changes in dello.js

--- hello.js
+++ hello.js
@@ -2,7 +2,7 @@ const express = require ('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello World [Node.js, awsEc2 and Docker]')
+    res.send('Hello World [Node.js, AwsEc2 and Dockers]')
 })
 
 app.listen(3000, () => {
