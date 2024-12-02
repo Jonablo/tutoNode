f5a2c7243606f9ba9ceda3acd9ca1c227d39b052
Jonablo <138418199+Jonablo@users.noreply.github.com>
2024-12-02 16:27:54 -0500

    Rename yml for docker deploy.

--- hello.js
+++ hello.js
@@ -2,7 +2,7 @@ const express = require ('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello Guys [Node.js, AwsEc2 and Dockers]')
+    res.send('Hello Mundo [Node.js, AwsEc2 and Dockers]')
 })
 
 app.listen(3000, () => {
